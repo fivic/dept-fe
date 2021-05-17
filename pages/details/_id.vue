f18503de1afdabf2a4887b819dbaf1bce2ec58e5
Fico <ivicfilip94@gmail.com>
2021-05-18 00:51:47 +0200

    ADD details page

---
+++ pages/details/_id.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="w-full border border-black p-2">
+      <NuxtLink to="/"><div class="bg-black w-6 h-6"></div></NuxtLink>
+      <div v-if="client" class="border border-black mt-4 p-2">
+        <VImage img="rainbow" md></VImage>
+        <p class="font-bold">{{ getName }}</p>
+        <div class="mt-2">
+          <p>Date of birth: {{ client.dob.date }}</p>
+          <p>
+            Place of residence:
+            {{ client.location.city + ', ' + client.location.country }}
+          </p>
+          <p>
+            E-mail: <a :href="`mailto:${client.email}`">{{ client.email }}</a>
+          </p>
+          <p>
+            Phone number:
+            <a :href="`tel:${client.phone}`">{{ client.phone }}</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VImage from '@/components/VImage'
+export default {
+  components: {
+    VImage,
+  },
+  data() {
+    return {
+      client: null,
+    }
+  },
+  computed: {
+    getName() {
+      return `${this.client.name.title} ${this.client.name.first} ${this.client.name.last}`
+    },
+  },
+  async mounted() {
+    this.client = await this.$store.getters['clients/getAll'].find(
+      (el) => el.id.value === this.$route.params.id
+    )
+    console.log('this.client', this.client)
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/detailsss.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="w-full border border-black p-2">
+      <NuxtLink to="/"><div class="bg-black w-6 h-6"></div></NuxtLink>
+      <div class="border border-black mt-4 p-2">
+        <VImage img="rainbow" sm></VImage>
+        <p class="font-semibold">Mrs Jane Doe</p>
+        <div class="mt-2">
+          <p>Date of birth</p>
+          <p>Place of residence</p>
+          <p>E-mail</p>
+          <p>Phone number</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VImage from '@/components/VImage'
+export default {
+  components: {
+    VImage,
+  },
+  data() {
+    return {
+      client: [],
+    }
+  },
+  async mounted() {
+    const clientData = await this.$axios.get('https://randomuser.me/api/')
+    console.log(clientData.data.results[0])
+  },
+}
+</script>
+
+<style></style>
