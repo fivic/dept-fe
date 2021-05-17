fb641aad3a6e81253531980dac494a03f07f1cb2
Fico <ivicfilip94@gmail.com>
2021-05-17 22:57:22 +0200

    ADD components

---
+++ components/VIcon.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex items-center">
+    <component
+      :is="svgLoader"
+      :class="{
+        'w-full': wfull,
+        'h-4 w-4': sm,
+        'h-6 w-6': md || (!sm && !lg && !xl),
+        'h-8 w-8': lg,
+        'h-10 w-10': xl,
+      }"
+    >
+    </component>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VIcon',
+  props: {
+    /** Icon name, svg file in '/assets/svg' */
+    icon: {
+      type: String,
+      default: 'logo',
+    },
+    /** Component takes full container width */
+    wfull: {
+      type: Boolean,
+      default: false,
+    },
+    /** Small size */
+    sm: {
+      type: Boolean,
+      default: false,
+    },
+    /** Medium size */
+    md: {
+      type: Boolean,
+      default: false,
+    },
+    /** Large size */
+    lg: {
+      type: Boolean,
+      default: false,
+    },
+    /** Extra large size */
+    xl: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    svgLoader() {
+      return () => import(`@/assets/svg/${this.icon}.svg?inline`)
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ components/VImage.vue
@@ -0,0 +1,63 @@
+<template>
+  <img
+    :src="imgLoader"
+    :class="{
+      'w-full': wfull,
+      'h-24': sm,
+      'h-32': md || (!sm && !lg && !xl),
+      'h-48': lg,
+      'h-64': xl,
+    }"
+    :alt="alt"
+  />
+</template>
+
+<script>
+export default {
+  name: 'VImage',
+  props: {
+    /** Image name, jpg file in '/assets/img/' */
+    img: {
+      type: String,
+      default: '360',
+    },
+    /** Component takes full container width */
+    wfull: {
+      type: Boolean,
+      default: false,
+    },
+    /** Small size */
+    sm: {
+      type: Boolean,
+      default: false,
+    },
+    /** Medium size */
+    md: {
+      type: Boolean,
+      default: false,
+    },
+    /** Large size */
+    lg: {
+      type: Boolean,
+      default: false,
+    },
+    /** Extra large size */
+    xl: {
+      type: Boolean,
+      default: false,
+    },
+    /** Image alt text */
+    alt: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    imgLoader() {
+      return require(`@/assets/img/${this.img}.jpg`)
+    },
+  },
+}
+</script>
+
+<style></style>
