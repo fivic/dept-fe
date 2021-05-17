7caf9119b662eb42283e5398fed327350c7be4ca
Fico <ivicfilip94@gmail.com>
2021-05-17 22:58:43 +0200

    FIX layout UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div class="pt-1">
+    <VIcon wfull icon="logo"></VIcon>
     <Nuxt />
   </div>
 </template>
+
+<script>
+import VIcon from '@/components/VIcon'
+export default {
+  components: {
+    VIcon,
+  },
+}
+</script>
 
 <style>
 html {
@@ -22,34 +32,5 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
